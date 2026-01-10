4e8e6866b3c31c595042474529de0da632f9605e
abluvionn <sabyraibika@gmail.com>
2026-01-10 18:11:58 +0600

    Create Our mission section on About us page

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,11 +1,13 @@
 import AboutUsBanner from '@/components/AboutUsBanner';
 import OurHistory from '@/components/OurHistory';
+import OurMissionSection from '@/components/OurMissionSection';
 
 const page = () => {
   return (
     <main>
       <AboutUsBanner />
       <OurHistory />
+      <OurMissionSection />
     </main>
   );
 };
---
+++ components/OurMissionSection.tsx
@@ -0,0 +1,20 @@
+const OurMissionSection = () => {
+  return (
+    <section className='pt-[30px] px-[30px] pb-[280px] our-mission lg:py-[87px]'>
+      <div className='max-w-7xl mx-auto flex flex-col items-end'>
+        <h2 className='text-[#090A3F] font-poppins font-bold text-2xl mb-7 lg:text-4xl'>
+          Our Mission
+        </h2>
+        <p className='text-[#02033B] font-poppins text-sm leading-6 text-justify tracking-wide max-w-[510px] lg:text-lg lg:leading-8'>
+          At Spasex Ltd, our mission is to deliver safe, reliable, and efficient
+          transportation solutions across the United States. We are committed to
+          exceeding customer expectations through continuous innovation,
+          adherence to the highest safety standards, and fostering a supportive
+          environment for our drivers and staff.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default OurMissionSection;
