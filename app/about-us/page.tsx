e9c7e7041af761388c63c0ab31d797a13400d849
abluvionn <sabyraibika@gmail.com>
2026-01-10 17:44:51 +0600

    Create Our history section

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,9 +1,11 @@
 import AboutUsBanner from '@/components/AboutUsBanner';
+import OurHistory from '@/components/OurHistory';
 
 const page = () => {
   return (
     <main>
       <AboutUsBanner />
+      <OurHistory />
     </main>
   );
 };
