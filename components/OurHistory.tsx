4973bd781222f6b7404e5bce265ea8fc43a51e75
abluvionn <sabyraibika@gmail.com>
2026-01-10 17:54:19 +0600

    Fix our history section on larger screens

--- components/OurHistory.tsx
+++ components/OurHistory.tsx
@@ -1,21 +1,23 @@
 const OurHistory = () => {
   return (
-    <section className='bg-[#090A3F] text-white pt-[30px] px-[30px] pb-[360px] our-history'>
-      <h2 className='font-poppins font-bold text-2xl mb-7'>Our History</h2>
-      <p className='font-poppins text-sm leading-6 text-justify tracking-wide'>
-        Spasex Ltd began as a modest regional freight service with a single
-        truck and a clear mission: to provide reliable and efficient
-        transportation solutions across Kyrgyzstan and Central Asia. Recognizing
-        the growing demand for dependable logistics, the company rapidly
-        expanded its fleet and services.
-      </p>
-      <p className='font-poppins text-sm leading-6 text-justify tracking-wide'>
-        By 2015, Spasex Ltd had established a reputation for punctual deliveries
-        and exceptional customer service, leading to partnerships with major
-        industries, including agriculture, construction, and retail. This growth
-        was fueled by investments in modern vehicles and driver training
-        programs, ensuring safety and efficiency on every journey
-      </p>
+    <section className='bg-[#090A3F] text-white pt-[30px] px-[30px] pb-[360px] our-history lg:py-[87px]'>
+      <div className='max-w-7xl mx-auto'>
+        <h2 className='font-poppins font-bold text-2xl mb-7 lg:text-4xl'>
+          Our History
+        </h2>
+        <p className='font-poppins text-sm leading-6 text-justify tracking-wide max-w-[620px] lg:text-lg lg:leading-8'>
+          Spasex Ltd began as a modest regional freight service with a single
+          truck and a clear mission: to provide reliable and efficient
+          transportation solutions across Kyrgyzstan and Central Asia.
+          Recognizing the growing demand for dependable logistics, the company
+          rapidly expanded its fleet and services. <br /> By 2015, Spasex Ltd
+          had established a reputation for punctual deliveries and exceptional
+          customer service, leading to partnerships with major industries,
+          including agriculture, construction, and retail. This growth was
+          fueled by investments in modern vehicles and driver training programs,
+          ensuring safety and efficiency on every journey
+        </p>
+      </div>
     </section>
   );
 };
