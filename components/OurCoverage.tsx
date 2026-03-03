79d325a3202786779b8cf7593505eb9b072a43df
abluvionn <sabyraibika@gmail.com>
2026-03-03 14:53:28 +0600

    Replace coverage on map image with embedded google maps

--- components/OurCoverage.tsx
+++ components/OurCoverage.tsx
@@ -20,13 +20,13 @@ const OurCoverage = () => {
           efficiency and care. We’re constantly expanding—so wherever you need
           us, we’re getting closer
         </p>
-        <Image
-          src='/images/our-coverage.png'
-          alt='us map'
-          width={306}
-          height={220}
-          className='mx-auto mt-10 lg:mt-15 lg:w-[821px] lg:h-[589px]'
-        />
+        <iframe
+          src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6044.667237344281!2d-73.9828925589817!3d40.75468647424072!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c259003c32025f%3A0x3640f353d26bbd07!2s500%20Fifth%20Avenue%2C%20500%205th%20Ave%20Suite%20400%2C%20New%20York%2C%20NY%2010110%2C%20USA!5e0!3m2!1sen!2skg!4v1772526807265!5m2!1sen!2skg'
+          className='mx-auto mt-10 lg:mt-15 w-full h-[250px] lg:h-[600px] rounded-xl'
+          allowFullScreen={true}
+          loading='lazy'
+          referrerPolicy='no-referrer-when-downgrade'
+        ></iframe>
       </div>
     </section>
   );
